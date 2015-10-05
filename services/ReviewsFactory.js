32c0034a68cbc557f2b768278c01a63d94994a25
Annie <anniehoogendoorn@gmail.com>
2015-10-05 16:43:56 -0700

    Put back in css header.

--- services/ReviewsFactory.js
+++ services/ReviewsFactory.js
@@ -1,6 +1,17 @@
 nicksFlicks.factory('ReviewsFactory', function ReviewsFactory() {
   var factory ={};
-  factory.reviews = [{ name: "Hackers is the best ever", id: 1, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }];
+  factory.reviews = [
+  {
+    name: "Hackers is the best ever",
+    id: 1,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    name: "Don't ever see Garden State",
+    id: 2,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  }
+  ];
 
   factory.addReview = function() {
     factory.reviews.push({ name: factory.reviewName,
